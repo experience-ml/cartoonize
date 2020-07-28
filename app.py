ede50bb7e1aa4f8f8d003ea0c3cbc9b8d37c7816
Tejas Mahajan <tejas.mahajan121@gmail.com>
2020-07-28 23:27:38 +0530

    Updates

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ import os
 import io
 import uuid
 import sys
-import time
-import subprocess
-import argparse
+import yaml
 import traceback
+
+with open('./config.yaml', 'r') as fd:
+    opts = yaml.safe_load(fd)
 
 sys.path.insert(0, './white_box_cartoonizer/')
 
@@ -14,17 +15,17 @@ from flask import Flask, render_template, make_response, flash
 import flask
 from PIL import Image
 import numpy as np
-# Google cloud storage
-from google.cloud import storage
-from google.cloud.storage.blob import Blob
-# Algorithmi (GPU inference)
-import Algorithmia
 
 from cartoonize import WB_Cartoonize
 
-from image_utils import *
-from gcloud_utils import upload_blob, generate_signed_url, delete_blob
-from video_api import api_request
+if not opts['run_local']:
+    if 'GOOGLE_APPLICATION_CREDENTIALS' in os.environ:
+        from gcloud_utils import upload_blob, generate_signed_url, delete_blob
+    else:
+        raise Exception("GOOGLE_APPLICATION_CREDENTIALS not set in environment variables")
+    from video_api import api_request
+    # Algorithmia (GPU inference)
+    import Algorithmia
 
 app = Flask(__name__)
 
@@ -32,23 +33,36 @@ app.config['UPLOAD_FOLDER_VIDEOS'] = 'static/uploaded_videos'
 app.config['CARTOONIZED_FOLDER'] = 'static/cartoonized_images'
 app.secret_key = b'casualbakarwadi'
 
-# parser = argparse.ArgumentParser()
-# parser.add_argument('--run_local', type=int, default=0, help='Set to 1 to run video API locally')
-# opts = parser.parse_args()
-
-opts = argparse.Namespace()
-# opts = {"run_local": 0}
-opts.run_local = 1
 app.config['OPTS'] = opts
 
 ## Init Cartoonizer and load its weights 
-wb_cartoonizer = WB_Cartoonize(os.path.abspath("white_box_cartoonizer/saved_models/"))
+wb_cartoonizer = WB_Cartoonize(os.path.abspath("white_box_cartoonizer/saved_models/"), opts['gpu'])
+
+def convert_bytes_to_image(img_bytes):
+    """Convert bytes to numpy array
+
+    Args:
+        img_bytes (bytes): Image bytes read from flask.
+
+    Returns:
+        [numpy array]: Image numpy array
+    """
+    
+    pil_image = Image.open(io.BytesIO(img_bytes))
+    if pil_image.mode=="RGBA":
+        image = Image.new("RGB", pil_image.size, (255,255,255))
+        image.paste(pil_image, mask=pil_image.split()[3])
+    else:
+        image = pil_image.convert('RGB')
+    
+    image = np.array(image)
+    
+    return image
 
 @app.route('/')
 @app.route('/cartoonize', methods=["POST", "GET"])
 def cartoonize():
     opts = app.config['OPTS']
-    input_fname_uri = ''
     if flask.request.method == 'POST':
         try:
             if flask.request.files.get('image'):
@@ -64,7 +78,7 @@ def cartoonize():
                 cartoonized_img_name = os.path.join(app.config['CARTOONIZED_FOLDER'], img_name + ".jpg")
                 cv2.imwrite(cartoonized_img_name, cv2.cvtColor(cartoon_image, cv2.COLOR_RGB2BGR))
                 
-                if not opts.run_local:
+                if not opts["run_local"]:
                     # Upload to bucket
                     output_uri = upload_blob("cartoonized_images", cartoonized_img_name, img_name + ".jpg", content_type='image/jpg')
 
@@ -87,7 +101,7 @@ def cartoonize():
                 width_resize=480
                 os.system("ffmpeg -hide_banner -loglevel warning -ss 0 -i '{}' -t 10 -filter:v scale={}:-2 -r 15 -c:a copy '{}'".format(os.path.abspath(original_video_path), width_resize, os.path.abspath(modified_video_path)))
                 
-                if opts.run_local:
+                if opts["run_local"]:
                     # if local then "output_uri" is a file path
                     output_uri = wb_cartoonizer.process_video(modified_video_path)
                 else:
@@ -99,12 +113,10 @@ def cartoonize():
                     output_uri = response['output_uri']
                 
                 # Delete the videos from local disk
-                os.system("rm " + original_video_path)
+                os.system("rm " + original_video_path) 
+                os.system("rm " + modified_video_path)
                 
-                # Put in IF BLOCK for local run, has to be present to display video. UnComment if you run this locally.
-                #os.system("rm " + modified_video_path)
-                
-                if opts.run_local:
+                if opts["run_local"]:
                     signed_url = output_uri
                 else:
                     signed_url = generate_signed_url(output_uri)
@@ -112,6 +124,7 @@ def cartoonize():
                 return render_template("index_cartoonized.html", cartoonized_video=signed_url)
         
         except Exception as e:
+            print(traceback.print_exc())
             flash("Our server hiccuped :/ Please upload another file! :)")
             return render_template("index_cartoonized.html")
     else:
